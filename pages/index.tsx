9c981778c9075ef8bf4ab88c22c8ab5b4b8ed65d
ggjiny <jnss599@gmail.com>
2024-03-31 22:14:40 +0900

    feat: Input 컴포넌트 페어프로그래밍 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,49 @@
+import Input from '@/components/common/form/Input';
+import { useState } from 'react';
+
 export default function Home() {
+  const [userInput, setUserInput] = useState({
+    email: '',
+    pw: '',
+    pwValid: '',
+  });
+
+  const handleInputChange = (name: string, value: string) => {
+    setUserInput({
+      ...userInput,
+      [name]: value,
+    });
+  };
   return (
     <>
-      <div>hi</div>
+      <form>
+        <label htmlFor="email">이메일</label>
+        <Input
+          name="email"
+          type="email"
+          value={userInput.email}
+          onChange={handleInputChange}
+          placeholder="이메일 입력"
+        />
+
+        <label htmlFor="pw">비밀번호</label>
+        <Input
+          name="pw"
+          type="password"
+          value={userInput.pw}
+          onChange={handleInputChange}
+          placeholder="비밀번호 입력"
+        />
+
+        <label htmlFor="pwValid">비밀번호 확인</label>
+        <Input
+          name="pwValid"
+          type="password"
+          value={userInput.pwValid}
+          onChange={handleInputChange}
+          placeholder="비밀번호 확인 입력"
+        />
+      </form>
     </>
   );
 }
